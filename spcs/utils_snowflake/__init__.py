a9b9925a3e75a8e87f2274c44d3ea983e323d31c
gargiasth <gargi.asthana@axleinfo.com>
2026-08-25 12:18:13 -0400

    feat: add upload to stages functionality

--- spcs/utils_snowflake/__init__.py
+++ spcs/utils_snowflake/__init__.py
@@ -1,5 +1,5 @@
 from .connection import get_connection
-from .execute_sql_file import execute_statements, run_sql_file, run_native_sql_file
+from .execute_sql_file import execute_statements, run_sql_file
 from .sql_translation import translate_postgres_to_snowflake
 from .upload_to_stage import upload_directory_to_stage
 
@@ -7,7 +7,7 @@ __all__ = [
     "get_connection",
     "execute_statements",
     "run_sql_file",
-    "run_native_sql_file",
+    "get_snowpark_session",
     "translate_postgres_to_snowflake",
     "upload_directory_to_stage",
 ]
