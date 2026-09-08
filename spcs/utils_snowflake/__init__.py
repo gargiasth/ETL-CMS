5cdda03381dd893ca4e52805c409833ef7beba18
gargiasth <gargi.asthana@axleinfo.com>
2026-09-08 10:59:22 -0400

    feat: add load to table utility to upload data from stage to tables

--- spcs/utils_snowflake/__init__.py
+++ spcs/utils_snowflake/__init__.py
@@ -2,6 +2,7 @@ from .connection import get_connection
 from .execute_sql_file import execute_statements, run_sql_file
 from .sql_translation import translate_postgres_to_snowflake
 from .upload_to_stage import upload_directory_to_stage
+from .load_to_table import copy_into_table, ensure_file_format
 
 __all__ = [
     "get_connection",
@@ -10,4 +11,6 @@ __all__ = [
     "get_snowpark_session",
     "translate_postgres_to_snowflake",
     "upload_directory_to_stage",
+    "copy_into_table",
+    "ensure_file_format"
 ]
